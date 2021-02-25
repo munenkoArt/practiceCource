8d2c0ef72369ff7d7cd7883c8327f43c5a51b88d
SimpleS <munenko17@ukr.net>
2021-02-25 12:02:11 +0200

    Conditions practice

--- practice/js/script.js
+++ practice/js/script.js
@@ -1,22 +1,46 @@
 
 "use strict";
 
-const namberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const namberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
-    count: namberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    privat: false
-};
+// let personalMovieDB = {
+//     count: namberOfFilms,
+//     movies:{},
+//     actors:{},
+//     genres:[],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотреных ильмов?', ''),
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотреных ильмов?', ''),
-      d = +prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотреных ильмов?', ''),
+//       b = +prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотреных ильмов?', ''),
+//       d = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
+// if(4 == 5){
+//     console.log('ok!!');
+// }
+// else{
+//     console.log('not ok!!');
+// }
+
+// (num === 3) ? console.log('ok!!') : console.log('not ok');
+
+let num = 5;
+
+switch(num){
+    case 4:
+        console.log('not ok');
+        break;
+    case 6:
+        console.log('ok');
+        break; 
+    default:
+        console.log('heeet');
+        break;       
+}
