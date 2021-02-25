0d365e451a5541dace017058beeb547d61c1bfb2
SimpleS <munenko17@ukr.net>
2021-02-25 11:43:59 +0200

    first practice with js

--- practice/js/script.js
+++ practice/js/script.js
@@ -1,4 +1,22 @@
 
+"use strict";
 
+const namberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const nameOfFilms = [];
+let personalMovieDB = {
+    count: namberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотреных ильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотреных ильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
